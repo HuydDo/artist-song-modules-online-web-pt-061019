8dd783a95ebdb0a3293cf93c35bf3b020ebe3f2f
Huy Do <huyddo40@gmail.com>
2019-07-01 19:40:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 class Artist
-  extend Memorable
-  include Paramable
-
+  # extend Memorable
+  # include Paramable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
